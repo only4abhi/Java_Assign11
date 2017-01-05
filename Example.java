60ea6f318af90a1f007122929ae6fe591fa5c45f
only4abhi <abhishekchanda.mailbox@gmail.com>
2017-01-05 17:45:42 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,10 @@
+
+public class Example {
+
+	 int x; int y;
+	 String name;
+	 public static void main(String args[]) {
+		 Example pnt = new Example();
+	 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	}
+}
